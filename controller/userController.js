aaeb14356ab230178f8e978354238d93448e7d1c
Rohan Ahmad <rohanse.1.ra@gmail.com>
2024-06-26 20:03:48 +0500

    update controller

--- controller/userController.js
+++ controller/userController.js
@@ -42,7 +42,7 @@ const userReg = async(req,res)=>{
 }
 
 const userRegPage = (req,res)=>{
-        res.render('signup',{error:req.flash('error')})
+        res.render('../views/welcome',{error:req.flash('error')})
 
     
 }
